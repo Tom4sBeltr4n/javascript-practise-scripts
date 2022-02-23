2ba41e24f2c63228071ab1f3d5f71cc95b65d3b4
Tom4sBeltr4n <tomasfelipe29@hotmail.com>
2022-02-23 08:48:22 -0500

    Began new kata and renamed previous one

---
+++ CURRENT-kata--sum-of-the-first-nth-term.js
@@ -0,0 +1,9 @@
+function SeriesSum(n)
+{
+  // https://www.codewars.com/kata/555eded1ad94b00403000071/train/javascript
+  console.log(n);
+  
+  let result;
+  result = Math.round(result)
+  return result
+}
